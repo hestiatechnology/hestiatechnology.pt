6ad3484939fa63ce40bb85d8558b5dd6ae9353a8
danielalexis <danielalexis.o.pereira@gmail.com>
2024-04-15 03:10:39 +0100

    Add basic design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from "./homepage/homepage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',  pathMatch: 'full', component: HomepageComponent},
+  //{ path: 'home', loadChildren: './home/home.module#HomeModule' },
+  //{ path: 'about', loadChildren: './about/about.module#AboutModule' },
+  //{ path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss'
+})
+
+export class HomepageComponent {
+
+}
