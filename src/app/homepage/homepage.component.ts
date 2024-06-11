657e65c6a484abcbe3db4dad9e76f9ec81b9b41d
danielalexis <danielalexis.o.pereira@gmail.com>
2024-06-11 19:01:55 +0100

    Add typeforn

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
-
+import { createWidget } from '@typeform/embed'
+import '@typeform/embed/build/css/widget.css'
 @Component({
   selector: 'app-homepage',
   standalone: true,
@@ -12,10 +13,18 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './homepage.component.scss'
 })
 
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
   //showAlert = true;
   //
   //hideAlert() {
   //  this.showAlert = false;
   //}
+
+  ngOnInit(): void {
+
+    const contactForm = document.getElementById('contactform')
+    console.log(contactForm)
+    if (!contactForm) return
+    createWidget('ytY4mJiA', { container: contactForm })
+  }
 }
