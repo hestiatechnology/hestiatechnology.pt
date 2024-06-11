e59791fc0d2426be6c97129b6bb716735b50467a
danielalexis <danielalexis.o.pereira@gmail.com>
2024-06-11 16:30:27 +0100

    chore: Remove unused CheckboxOverviewExample component from app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from "./homepage/homepage.component";
 import { PriceCalculatorComponent } from "./price-calculator/price-calculator.component";
-import { CheckboxOverviewExample } from "./example/checkbox-overview-example";
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomepageComponent },
   { path: 'calculator', component: PriceCalculatorComponent },
-  { path: 'example', component: CheckboxOverviewExample },
   //{ path: 'home', loadChildren: './home/home.module#HomeModule' },
   //{ path: 'about', loadChildren: './about/about.module#AboutModule' },
   //{ path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
