bbf2d73b2e60ac95393c4c14cb6858d58db322cb
danielalexis <danielpereira@hestiatechnology.pt>
2026-01-29 15:27:05 +0000

    revert config

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,13 +30,6 @@ export default defineConfig({
   ],
 
   vite: {
-      resolve: {
-            // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-            // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-            alias: import.meta.env.PROD && {
-                'react-dom/server': 'react-dom/server.edge',
-            },
-        },
     plugins: [tailwindcss()],
   },
 
